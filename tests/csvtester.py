fd64599c39e61ea48b242fa614930ecdd4f24487
Justin Schmidt <justin7@gmail.com>
2018-03-06 09:35:37 -0500

    some restructuring

---
+++ tests/csvtester.py
@@ -0,0 +1,7 @@
+# Test:
+# 1 First row is header row, starting with #
+# 2 Each header name is unique
+# 3 No special characters in header names.  Letters, numbers, underscores.  Starts with letter.
+# 4 No empty rows
+# 5 Each row fills all columns
+# 6 File ends in "s.csv"
